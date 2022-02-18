01b7a70145bdb9423f99656a52901001ed079718
unknown <stahanovgod@gmail.com>
2022-02-18 13:05:02 +0500

    put theme into localStorage

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,15 +1,19 @@
 import React, {ReactNode, useState} from "react";
 import {Theme, ThemeContext} from "../context/ThemeContext";
 import {changeCSSRootVariables} from "../model/ChangeCSSRootVariables";
+import {storage} from "../model/Storage";
 
 interface Props {
     children: ReactNode
 }
 
 export const ThemeProvider = ({children, ...props}: Props) => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const [theme, setTheme] = useState<Theme>(storage.getItem('theme') || Theme.LIGHT);
+
+    changeCSSRootVariables(theme);
 
     const changeTheme = (theme: Theme) => {
+        storage.setItem('theme', theme);
         setTheme(theme);
         changeCSSRootVariables(theme);
     }
